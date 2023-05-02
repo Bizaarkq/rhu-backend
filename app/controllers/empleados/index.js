dfc0409b93b186a5706cf20f50ab573e0668be8c
Bizaark <isaac.bernab.01@gmail.com>
2023-05-01 21:39:27 -0600

    🐛 some fixes para buscar y crear empleados

--- app/controllers/empleados/index.js
+++ app/controllers/empleados/index.js
@@ -20,7 +20,12 @@ exports.crearEmpleado = async (req, res) => {
             datos_afiliacion
         } = req.body;
 
+        const last_codigo = await Empleados.find({}).sort({codigo: -1}).limit(1);
+
         let empleado = new Empleados({
+            codigo: last_codigo.length > 0 ? 
+                (parseInt(last_codigo[0].codigo) + 1).toString().padStart(3, '0')
+                : '001',
             datos_personales: {
                 nombres: datos_personales.nombres,
                 apellidos: datos_personales.apellidos,
@@ -45,7 +50,10 @@ exports.crearEmpleado = async (req, res) => {
                 tipo_cuenta: datos_bancarios.tipo_cuenta
             },
             datos_afiliacion: {
-                afp: datos_afiliacion.afp,
+                afp: {
+                    afiliacion: datos_afiliacion.afp.afiliacion,
+                    numero: datos_afiliacion.afp.numero
+                },
                 isss: datos_afiliacion.isss
             }
         });
@@ -57,6 +65,7 @@ exports.crearEmpleado = async (req, res) => {
         });
 
     }catch (e) {
+        console.log(e);
         return res.status(500).json({
             ok: false,
             message: 'Error interno del servidor'
@@ -96,7 +105,7 @@ exports.obtenerEmpleado = async (req, res) => {
 
 exports.obtenerEmpleados = async (req, res) => {
     try {
-        let empleado = await Empleados.find({});
+        let empleado = await Empleados.find({}).populate('datos_laborales.cargo');
         return res.status(200).json({
             ok: true,
             message: 'Empleados encontrados',
