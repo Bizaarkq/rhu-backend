4a83a6a13fd7b1df2483262fb4a4ab50391b2a36
Bizaark <isaac.bernab.01@gmail.com>
2023-05-10 23:19:38 -0600

    ✨ se actualizan metodos de incapacidades y se agrega la obtención y eliminación de incapacidades

--- app/controllers/incapacidades/index.js
+++ app/controllers/incapacidades/index.js
@@ -1,15 +1,25 @@
 const Empleados = require('../../models').Empleados;
+const moment = require('moment');
 
 exports.registrarIncapacidad = (req, res) => {
-    const { id_empleado, fecha, dias, motivo } = req.body;
+    const {
+      empleado: { value },
+      fecha,
+      dias,
+      motivo,
+      observaciones,
+      remunerado
+    } = req.body;
 
-    Empleados.findOne({_id: id_empleado}).then((empleado) => {
+    Empleados.findOne({_id: value}).then((empleado) => {
         if(!empleado) return res.status(404).json({ ok: false, message: 'Empleado no encontrado' });
 
         empleado.incapacidades.push({
             fecha,
             dias,
-            motivo
+            motivo,
+            observaciones,
+            remunerado
         });
 
         empleado.save().then((empleado) => {
@@ -44,7 +54,7 @@ exports.obtenerIncapacidades = (req, res) => {
                     return {
                         id_incapacidad: incapacidad._id,
                         fecha: incapacidad.fecha,
-                        fecha_fin: new Date(incapacidad.fecha.getTime() + (incapacidad.dias * 24 * 60 * 60 * 1000)),
+                        fecha_fin: new Date(incapacidad.fecha.getTime() + ((incapacidad.dias - 1) * 24 * 60 * 60 * 1000)),
                         dias: incapacidad.dias,
                         motivo: incapacidad.motivo
                     }
@@ -61,16 +71,16 @@ exports.obtenerIncapacidades = (req, res) => {
 exports.editarIncapacidad = (req, res) => {
     try {
         let { 
-            id_empleado,
-            id_incapacidad,
+            empleado: {value},
             fecha,
             dias,
             motivo,
             observaciones,
-            remunerado
+            remunerado,
+            id_incapacidad
          } = req.body;
     
-        Empleados.findOne({_id: id_empleado}).then((empleado) => {
+        Empleados.findOne({_id: value}).then((empleado) => {
     
             if(!empleado) return res.status(404).json({ 
                 ok: false, 
@@ -104,6 +114,90 @@ exports.editarIncapacidad = (req, res) => {
                 });
             });
     
+        });
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({ 
+            ok: false, 
+            message: 'Error interno del servidor', 
+            err 
+        });
+    }
+}
+
+exports.obtenerIncPorFecha = (req, res) => {
+    
+    try{
+        Empleados.find({}).then((empleados) => {
+            let eventos = [];
+            empleados.forEach((empleado) => {
+                empleado.incapacidades.forEach((incapacidad) => {
+                    eventos.push({
+                        title: empleado.codigo + ': ' + empleado.datos_personales.nombres + ' ' + empleado.datos_personales.apellidos,
+                        start: incapacidad.fecha,
+                        end: new Date(incapacidad.fecha.getTime() + ((incapacidad.dias -1) * 24 * 60 * 60 * 1000)),
+                        allDay: true,
+                        resource: {
+                            id_empleado: empleado._id,
+                            id_incapacidad: incapacidad._id,
+                            codigo: empleado.codigo,
+                            observaciones: incapacidad.observaciones,
+                            remunerado: incapacidad.remunerado,
+                            dias: incapacidad.dias,
+                            motivo: incapacidad.motivo,
+                            fecha_inicio: moment(incapacidad.fecha).format('DD/MM/YYYY'),
+                            fecha_fin: moment(incapacidad.fecha).add(incapacidad.dias - 1, 'days').format('DD/MM/YYYY'),
+                            fecha: moment(incapacidad.fecha).add(1, 'days').format('YYYY-MM-DD'),
+                            empleado: {
+                                label: empleado.datos_personales.nombres + ' ' + empleado.datos_personales.apellidos,
+                                value: empleado._id
+                            }
+                        }
+                    })
+                });
+            });
+
+            return res.status(200).json({
+                ok: true,
+                message: 'Incapacidades encontradas',
+                eventos
+            });
+        });
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({ 
+            ok: false, 
+            message: 'Error interno del servidor', 
+            err 
+        });
+    }
+}
+
+exports.eliminarIncapacidad = (req, res) => {
+    try{
+        const { id_empleado, id_incapacidad } = req.body;
+
+        Empleados.findOne({_id: id_empleado}).then((empleado) => {
+            if(!empleado) return res.status(404).json({ 
+                ok: false, 
+                message: 'Empleado no encontrado' 
+            });
+
+            empleado.incapacidades.pull({_id: id_incapacidad});
+
+            empleado.save().then((empleado) => {
+                return res.status(200).json({ 
+                    ok: true, 
+                    message: 'Incapacidad eliminada con éxito', 
+                    empleado 
+                });
+            }).catch((err) => {
+                return res.status(500).json({ 
+                    ok: false, 
+                    message: 'Error interno del servidor', 
+                    err 
+                });
+            });
         });
     }catch(e){
         console.log(e);
--- app/routes/incapacidades/index.js
+++ app/routes/incapacidades/index.js
@@ -2,8 +2,10 @@ const routes = require('express').Router();
 const controller = require('../../controllers').incapacidades;
 
 routes.post('/add', controller.registrarIncapacidad);
-routes.get('/:id_empleado', controller.obtenerIncapacidadesPorEmpleado);
+routes.get('/byEmpleado/:id_empleado', controller.obtenerIncapacidadesPorEmpleado);
 routes.get('/', controller.obtenerIncapacidades);
 routes.post('/edit', controller.editarIncapacidad);
+routes.get('/eventos', controller.obtenerIncPorFecha);
+routes.post('/delete', controller.eliminarIncapacidad);
 
 module.exports = routes;
